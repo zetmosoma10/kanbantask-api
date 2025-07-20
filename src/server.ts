a03683e6a7ee3d7cafead7b5d09f1145f845ed48
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-07-20 15:33:33 +0200

    set up express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./app";
+import { env } from "process";
 
+const port = env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running at port: ${port}`);
+});
