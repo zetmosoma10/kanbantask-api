5cffc82afed7893586172da3e9971b1fd58a6a1c
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-07-20 15:44:56 +0200

    connect express server to mongodb database

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,23 @@ dotenv.config();
 import app from "./app";
 import { env } from "process";
 
-const port = env.PORT || 3000;
+import mongoose from "mongoose";
 
-app.listen(port, () => {
-  console.log(`Server running at port: ${port}`);
+async function main() {
+  const dbUrl = env.KANBAN_DB_CONN_STR;
+  if (!dbUrl)
+    throw new Error("❌ Missing environment variable: KANBAN_DB_CONN_STR");
+
+  const port = env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server running in port:${port}...`);
+  });
+
+  await mongoose.connect(dbUrl);
+  console.log("Successfully connected to database...");
+}
+
+main().catch((err) => {
+  console.error("App failed to run.", err);
+  process.exit(1);
 });
