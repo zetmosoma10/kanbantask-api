f5a38f5d43321e57f443f698cf4b4a39b378f455
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-07-21 11:05:07 +0200

    create board schema

---
+++ src/models/Board.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const boardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      minlength: [3, "board name must be at least 3 characters."],
+      maxlength: [50, "board name must not exceeds 50 characters."],
+      required: [true, "board name is required."],
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "createdBy field is required."],
+    },
+  },
+  { timestamps: true }
+);
+
+boardSchema.index({ createdBy: 1 });
+boardSchema.index({ name: 1, createdBy: 1 }, { unique: true });
+
+const Board = mongoose.model("Board", boardSchema);
+
+export default Board;
