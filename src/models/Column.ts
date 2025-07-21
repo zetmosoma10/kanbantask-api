806e43df78da4933b9f632bb800fc3e9920763e0
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-07-21 12:52:04 +0200

    create column schema

---
+++ src/models/Column.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const columnSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      minlength: [3, "column name must be at least 3 characters."],
+      maxlength: [50, "column name must not exceeds 50 characters."],
+      required: [true, "column name is required."],
+    },
+    boardId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Board",
+    },
+  },
+  { timestamps: true }
+);
+
+columnSchema.index({ boardId: 1 });
+
+const Column = mongoose.model("Column", columnSchema);
+
+export default Column;
